672a9130fad70dfc9798acf15371baf975bf067b
matthewfripp <matthewfripp@gmail.com>
2022-04-16 12:09:46 +0100

    join fix

--- chrome/background.js
+++ chrome/background.js
@@ -5,7 +5,7 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, { url }) => {
     chrome.scripting.insertCSS({ target: { tabId }, files: ['styles.css'] });
     chrome.scripting.executeScript({ target: { tabId }, files: ['content.js'] });
 
-    const func = (place, id) => Roblox.GameLauncher.joinGameInstance(place, id)
-
-    chrome.runtime.onMessage.addListener(({ message }) => chrome.scripting.executeScript({ target: { tabId }, func, args: [message.place, message.id], world: 'MAIN' }));
 });
+
+const func = (place, id) => Roblox.GameLauncher.joinGameInstance(place, id)
+chrome.runtime.onMessage.addListener(({ message }, { tab }) => chrome.scripting.executeScript({ target: { tabId: tab.id }, func, args: [message.place, message.id], world: 'MAIN' }));
